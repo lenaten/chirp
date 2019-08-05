c968b99091f9388f967688d157e26406a441b54d
lenaten <maman.nathaniel@gmail.com>
2019-08-06 01:08:25 +0300

    .

---
+++ Chirp_Receive.ps1
@@ -0,0 +1,31 @@
+$CHIRP_APP_KEY = "cF2DFf70c4ACF71dFB9AceAb6"
+$CHIRP_APP_SECRET = "8E2bFf3C82a5ACA00DFE4B3d3Ed891BbEb99b99624b144dBFa"
+$CHIRP_APP_CONFIG = "HwMItXX2JvlT9T6sZtA0SycV/V2H58U6LSraSc4XJhIAKFudA5eMkgwIoI/aCpesGyDhTRzptj0melErz1EvWQvdpu/Jfi+oxVuuDrfH+7LrCdKoN4cZqauryNHN2bF5LfNuldJCNZVyUn7Eoz4l/J7LRqASTiXs6HTRRpoLLGjSM27WWtzDAnjaSbdUS3AIxVP3f+LhtDEj0u+e1fQVpxsZPRcenILKrZU6YgL986z9/xEaoyyFM/IZId16H7BsV8fe7WiT6lSlwsKMw3l45KzRBFSJpfe6laqlGKGGPfn/qORrwG7aFn4Q75qcs567e+ZTQzEDdiNJADgel+jgxZiA8B1D99SdKebBnT+Oy/WoWmk8iIBn61Tw+Oapqz55IlTAmSSbFr6/s5R8hW9i1uLaNIpoQN5gl+cGVB2/rdGiew87EoLy9agXSRu1UoJikgBP/dGeAHng3AotbXj0VIk06ofXjcp/+1osKVIquCFGvtem7T35TOl4hYvQ6po8cugWPkue8rh4+bHPdryxRfswe9r2vc0RJ7KbGjfBXm7NdEFI6wD4f/7Q+MpTtYEJMknCVLk/ph978LgWzQ5NvL+BPoVDa0JkPjxyXwTLz2/pqU1pOwGVG4K1pOFXigUyUrNEL/7o+/k3jmSgprjN6Y4uQevf0kfHvIuk65sE0ePIgZEKowfQGtvghitkUm++amVN1LC5J2Ap2LVP1fyH0jj1iQGhd2x8o8ERMyIxStF+2RMvGAG1wNpWhGAAQh6e4vZ5XEwwjY8dl4ssuqKrVV2H4wW4K5E9ywlDvpcDQHyCSDrW23E09YLgc6Iz4ois7nJ1ZWb/OTgyypRsY/VswEkjpN1Jc+xVoU5vNYdQWvgLRrkhq3RtsPvfTM7873u98LjTPJqVoXg0cJZ6KUHtBPR+B2oiIfI8lCpfvFrhMZA8SaqSy1QYuve4WNkSE4SfmeVu9yc5/T9qttTaaLsCVu7TJqUY2vPj6P07ckubAGyzD4K4HHH01li3/H/J0pyXA2qu3oDjC/WTEtQRILNuPWYESqAARdJX4XPrMLUcGzDJxgO1WjgOo9E4gh5OvCSipHI9KdAuCipu7k3gwSrmMkVdv2mlPRr/GlR07W/eiW/LA3jVCRCdPQ+0hf3QYHjklXWgUd1a3whoBsz8nO72t6yrH04SSLgVBAn5gyshquGfqSCWCCJtF9VrElsnFTHT/Ph3Wp7OevzhURBjgPrVxqk/kRX00uJmc/aa1OdP7xg8HgfZuq/hRoEQHYrRnlMGq5Aa+eyULtHjlTRSUtGt4EiWIn0Ey9OjUOkWV6w5YYvpbwehMg0vFSccanJ6lg1n+dSa3GHxEv6KL8zQ7uW7Qbf7t9SjQEntP2CeBQKwmQ9U0O4M8D/AGhT1A01mJp4VGjr3BqmHaexoZdPemuDzcibOTkCH1yl/VgiFKg1JK6DRf6+e84StToyqBVf09yjjR5JeOhSc+5oK4mo51jqucf3OzSVMzYZ3wU9Dct3DI//TbgYp806nckqWoZZ8x9vnoXpa6+eZNkMIE/7EYGe19R0Q7nHnAD9IyKtnU+LnLaY5XH7BNabJVEA954KeYyFPvFU9M4mIikxk3OUmAwXUGLFTPlxZFNDPmr3i41BR3Bh3XJ8d/bhNQxaUECpWd6YJ4WFYPvhkjhbFZn3QI8v55t4UH2dIKb3Am/RA1IRwOzC+9h5Hc+RLvhwRm+gjsJxMDBzcpzBfsJQMTyZUbur7EdqO1OS5Pppowlb2z0naLLreumt4qr+7IoTiNgfgNa9dbQzyaNSE6oyo1XtrWYVMsFVBbHeD6e6203SWKMyrDPxPfRJVUVKpI1rI8XB+FcCgpfsFe8kCBRPxPHavb2iMYJJbVuzJMmpzHSR5gtCzSx41Qt635tluc4N20+j5I/2pQKzW3UgR5HJR7SWN6wU/lKBvQ8WClPkgLdfpIYB3oMP1T8AmitjDltBquWTS46z0l1WVD6w0rfQdB4/OCi6Z9vJbLdJ59iSaWgeya9osQRkPaeo76sAVpUQgMLH/WlHkuTDpATi3SGYTiZjTrkF4pJSabxRG+6FZwFS3sDefQE9AmXjvz3Yi78qCbI1jxK0FUzDH/5LIMUs5R1RLDz0njC1DRY95e4eJmktWUfJkeEu47W+XGkUegUiDzW41vaD4aOny4pwlo6B5zs+BzFtrBRdIasEWwIUii75vesrlkqJNe5XeEEbHy6XcTQGvh3//bi8znpwHUXMxNv2UjMO2MKlTHe9B95vU4iM6vxeDF2OiCv/A0022lQapFIX+IT3AXaAr8NbbSvP8om5HSqGe3Rbq1i2SG+kbP2LANFCwWMpqLChaJo+WyXbKqi4U5dyewq5U1Z2z1xO4VoH1ybkZEDnIOziJCU5Ym2ycs74VfBOyx2Dho4QU3BLfb+QH1DMYTauovKpwoIRUr0Lj18jT5kd8rAqMvqPDyAP/qbwLgxIZeeorfjjZEBl0BbjMs0gglKr01Z0cZURU61aWSQDVaOwz8kRsoPGtBjNbZOTmv23Bb33/4ejvaCuHTr8B7JJAoBwPwJdlaobSQaPS2uE0Igux2+znIZzkGna+ugkD+Xw="
+
+$id = "neta"
+
+$AssemblyPath = Join-Path $PSScriptRoot 'ChirpConnectNet-x64.dll'
+[System.Reflection.Assembly]::LoadFile($AssemblyPath)
+$connect = [ChirpConnectNet.ChirpConnect]::new($CHIRP_APP_KEY, $CHIRP_APP_SECRET, $CHIRP_APP_CONFIG)
+$connect.AutoMute = $false
+Register-ObjectEvent -InputObject $connect -EventName "OnReceived" -SourceIdentifier $id
+# -Action {
+#     Write-Output $EventArgs
+# }
+$err = $connect.Start([ChirpConnectNet.AudioMode]::Receive)
+Write-Output $err
+$enc = [system.Text.Encoding]::UTF8
+
+
+try
+{
+    while ($true) {
+        $e = Wait-Event $id
+        Write-Output $e 
+        #  $enc.GetString($e.SourceArgs[0])
+        Start-Sleep -Seconds 5
+    }
+}
+finally {
+    Unregister-Event -SourceIdentifier $id
+}
