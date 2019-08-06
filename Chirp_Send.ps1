74744000bfd1b271c4dc5120108448558cd5da8c
lenaten <maman.nathaniel@gmail.com>
2019-08-06 23:54:14 +0300

    .

--- Chirp_Send.ps1
+++ Chirp_Send.ps1
@@ -1,17 +1,51 @@
-$CHIRP_APP_KEY = "cF2DFf70c4ACF71dFB9AceAb6"
-$CHIRP_APP_SECRET = "8E2bFf3C82a5ACA00DFE4B3d3Ed891BbEb99b99624b144dBFa"
-$CHIRP_APP_CONFIG = "HwMItXX2JvlT9T6sZtA0SycV/V2H58U6LSraSc4XJhIAKFudA5eMkgwIoI/aCpesGyDhTRzptj0melErz1EvWQvdpu/Jfi+oxVuuDrfH+7LrCdKoN4cZqauryNHN2bF5LfNuldJCNZVyUn7Eoz4l/J7LRqASTiXs6HTRRpoLLGjSM27WWtzDAnjaSbdUS3AIxVP3f+LhtDEj0u+e1fQVpxsZPRcenILKrZU6YgL986z9/xEaoyyFM/IZId16H7BsV8fe7WiT6lSlwsKMw3l45KzRBFSJpfe6laqlGKGGPfn/qORrwG7aFn4Q75qcs567e+ZTQzEDdiNJADgel+jgxZiA8B1D99SdKebBnT+Oy/WoWmk8iIBn61Tw+Oapqz55IlTAmSSbFr6/s5R8hW9i1uLaNIpoQN5gl+cGVB2/rdGiew87EoLy9agXSRu1UoJikgBP/dGeAHng3AotbXj0VIk06ofXjcp/+1osKVIquCFGvtem7T35TOl4hYvQ6po8cugWPkue8rh4+bHPdryxRfswe9r2vc0RJ7KbGjfBXm7NdEFI6wD4f/7Q+MpTtYEJMknCVLk/ph978LgWzQ5NvL+BPoVDa0JkPjxyXwTLz2/pqU1pOwGVG4K1pOFXigUyUrNEL/7o+/k3jmSgprjN6Y4uQevf0kfHvIuk65sE0ePIgZEKowfQGtvghitkUm++amVN1LC5J2Ap2LVP1fyH0jj1iQGhd2x8o8ERMyIxStF+2RMvGAG1wNpWhGAAQh6e4vZ5XEwwjY8dl4ssuqKrVV2H4wW4K5E9ywlDvpcDQHyCSDrW23E09YLgc6Iz4ois7nJ1ZWb/OTgyypRsY/VswEkjpN1Jc+xVoU5vNYdQWvgLRrkhq3RtsPvfTM7873u98LjTPJqVoXg0cJZ6KUHtBPR+B2oiIfI8lCpfvFrhMZA8SaqSy1QYuve4WNkSE4SfmeVu9yc5/T9qttTaaLsCVu7TJqUY2vPj6P07ckubAGyzD4K4HHH01li3/H/J0pyXA2qu3oDjC/WTEtQRILNuPWYESqAARdJX4XPrMLUcGzDJxgO1WjgOo9E4gh5OvCSipHI9KdAuCipu7k3gwSrmMkVdv2mlPRr/GlR07W/eiW/LA3jVCRCdPQ+0hf3QYHjklXWgUd1a3whoBsz8nO72t6yrH04SSLgVBAn5gyshquGfqSCWCCJtF9VrElsnFTHT/Ph3Wp7OevzhURBjgPrVxqk/kRX00uJmc/aa1OdP7xg8HgfZuq/hRoEQHYrRnlMGq5Aa+eyULtHjlTRSUtGt4EiWIn0Ey9OjUOkWV6w5YYvpbwehMg0vFSccanJ6lg1n+dSa3GHxEv6KL8zQ7uW7Qbf7t9SjQEntP2CeBQKwmQ9U0O4M8D/AGhT1A01mJp4VGjr3BqmHaexoZdPemuDzcibOTkCH1yl/VgiFKg1JK6DRf6+e84StToyqBVf09yjjR5JeOhSc+5oK4mo51jqucf3OzSVMzYZ3wU9Dct3DI//TbgYp806nckqWoZZ8x9vnoXpa6+eZNkMIE/7EYGe19R0Q7nHnAD9IyKtnU+LnLaY5XH7BNabJVEA954KeYyFPvFU9M4mIikxk3OUmAwXUGLFTPlxZFNDPmr3i41BR3Bh3XJ8d/bhNQxaUECpWd6YJ4WFYPvhkjhbFZn3QI8v55t4UH2dIKb3Am/RA1IRwOzC+9h5Hc+RLvhwRm+gjsJxMDBzcpzBfsJQMTyZUbur7EdqO1OS5Pppowlb2z0naLLreumt4qr+7IoTiNgfgNa9dbQzyaNSE6oyo1XtrWYVMsFVBbHeD6e6203SWKMyrDPxPfRJVUVKpI1rI8XB+FcCgpfsFe8kCBRPxPHavb2iMYJJbVuzJMmpzHSR5gtCzSx41Qt635tluc4N20+j5I/2pQKzW3UgR5HJR7SWN6wU/lKBvQ8WClPkgLdfpIYB3oMP1T8AmitjDltBquWTS46z0l1WVD6w0rfQdB4/OCi6Z9vJbLdJ59iSaWgeya9osQRkPaeo76sAVpUQgMLH/WlHkuTDpATi3SGYTiZjTrkF4pJSabxRG+6FZwFS3sDefQE9AmXjvz3Yi78qCbI1jxK0FUzDH/5LIMUs5R1RLDz0njC1DRY95e4eJmktWUfJkeEu47W+XGkUegUiDzW41vaD4aOny4pwlo6B5zs+BzFtrBRdIasEWwIUii75vesrlkqJNe5XeEEbHy6XcTQGvh3//bi8znpwHUXMxNv2UjMO2MKlTHe9B95vU4iM6vxeDF2OiCv/A0022lQapFIX+IT3AXaAr8NbbSvP8om5HSqGe3Rbq1i2SG+kbP2LANFCwWMpqLChaJo+WyXbKqi4U5dyewq5U1Z2z1xO4VoH1ybkZEDnIOziJCU5Ym2ycs74VfBOyx2Dho4QU3BLfb+QH1DMYTauovKpwoIRUr0Lj18jT5kd8rAqMvqPDyAP/qbwLgxIZeeorfjjZEBl0BbjMs0gglKr01Z0cZURU61aWSQDVaOwz8kRsoPGtBjNbZOTmv23Bb33/4ejvaCuHTr8B7JJAoBwPwJdlaobSQaPS2uE0Igux2+znIZzkGna+ugkD+Xw="
+[CmdletBinding()]
+param (
+    [string]$ChirpKey = "cF2DFf70c4ACF71dFB9AceAb6",
+    [string]$ChirpSecret = "8E2bFf3C82a5ACA00DFE4B3d3Ed891BbEb99b99624b144dBFa",
+    [string]$ChirpConfig = "nink6/zDqxzcPl7dm2KSZKVKsB2VTj1LMVoYokblJDKBPi0AMiRQv6k/1FpTX2XUZmyJ7LrYb7Hcpr3jNq8SwudoiAMmA03HLIlctRvvQdguh+2oN5qAhT6rVXgRK39SsbW7/TO+yQzL5zfYZ1LsiqHpoTfWM3+6yYv3v6mu2d/hj0MrbWs3eULVJoMudvo9t0Cg29IOOA8sz3UNPfWYSOSODTWiBd2lqXG/BBabizLxTKZ5RNvEbWY/Zud9V1/7eJZjQI1vivnt20YR9jR915QPrmgZ5vsvuOI+J3u/fozIoMh/hnOrEAZxqwzs3bO6CiDztqXeHfldwqe/NB6KMIQyy9O7wusjLirsvthBv0Nu/j1MYA9DqI67GvXOVTOLkieGn0f8Xi8T6Sg12g6BuJc6MuIukKEZPidE12G2dq8h+njZd33VR6L2hePSSP+j3f6dTu6sy2ZalqaODMiRf3YV+eyMKnNPM6JW6NRLV0R/kMBUlm+fN+sn2j0+vkKVGh0XTS/hI9kJyKOyfG0pnyYh7yX9mOWqnWZs1CBfaletBwNWoC1IaNHNWq1XCNcwYUTjITSLfF4IxE2JmeEAUdPxG2+t/1qLI0tmaDipFsaUDxcUdHseGHR/1qHEffqSIrUypELVoVkVqpquQeUyHLlocj/UUfixPr5MWFvNe1uSwDuz7TZpSBWXYS33+yV3HV/H89x7E2P3pqVkvXK29PHURsPRMW/SXihyjQCNw/e7bS92TZimXUZ/WcNbWX3+3sZtEy4vJRS6gbK9o+FUDzk6ncA4hA2+TVZRNEXFObWRMeuDKq3RntKnmVMHMn7jLHVseKJXAdlh1/gMMwnhAqztcZUk4XBINZh0N227ANMnUC/kWbEb5CHjcB7fosQ1/EUVHJofQ8gIlkuNT+lIzk3recByiUwc5HNSY+/bc6j7Ll2SH1BEKpJqNtir18rXTIc2ARnxi1njeKDJLgkgWMwFBoxuMwVCqQgkvYhvg7BVEmFTlMk4tq9g6BOPgC7Qo8QGrreGPaWUQ9Ot0Y5mOCC3VK7jsbrWmHltTchrZjYRIaWn0AMqqkSGCbPI/aA7shkBZiwBPa+8ngjGewG6WBfdW0fw0WzhL9bsbADFUx/xW4JQO2z5AQsEmCCXJNIQilYUK0QOKiyDNZAM4WLunTLtyPdpRLgvbQ0qOMHf+Vbdvedy1CgHx+WFGWe4e06Dn5Y8rEp4SvFpbQs9O76oDiLRa6cxHPOl7CA7uYFSR0zVAnFS8zGH748wPd0bi0AQMIsh0OIBJdIG8UBYjVej89WSy459C6IPcTdEFT7UCBsCQHH9HlGovaBlc/fgy6Hm0sCXuAs/RiMG26BF6Ma/yHSCOLHOoyD1VIX2Uj/7T9Cdl/H/okADWbuiIzD1sRXxKFOLPw+PAUHDuUTBBVQuT2Nxdbn0/Y+TuupUP4rLoWiEVdnnyTM91Cn66VomCsu05sckESZKbOXpbt5r5ip80WGR7VJrgSeS8u6wU60XFQc9UFJM0IqOTQ1RuGlXgDnApdFgWgHrxEG6nCoO7GY+QUaFrqsWvoFwBP/GGAPOIz6SjyA/jaigRk1sJajAVg2RW/6b6xbgu4tMYTXecvLQVCwqQBoSoPmVnfLrssIjy74kmO5D5Nbm3ODBBqT2+7bGuHPN1Cc/N0mLPWq+2Q0MBp3vMtvwg/CoyTmOWKO1S3BXbHKCaBdxX9U6/LzSzmJ0zikoF9Xsuk7nlzKe2rKsdY686mTMuPh+SX/U40Fn98Zv+H8FCah/mfv9xuS1foYj42udi3ehxJX3E2oGmLUKvXG2Mu2+PAY2D1/2RHeQZnq+J8oFcjwEX6Ad6uYIGak3m4iGE37CnmU57zXZ5hQktGVHbscUbMfgi2T6uOutgEioHdyJAKtsSDLHqgs26WajnCchAbAEGSd9Oszx/Cfqohw0e9isQ/DUY2lagkGvldx2ds9WugxRiJ/CNaT78P/zkgZL56JaWNoZABdoVW8lBsOdbaW8x5Zs6O5rQYa1tthOtNZyiL/uZCs2rHLry1JzonTpvGty3pk9HivRucMhWuYsM5PeXHaNmjB/P6qyTk/UWH0IPJ57GWkfWuOZPDaBUMTlJw357B+taewph05Hv9WLjR0ABKBKCZMSZem2oFkW5G+Jh/qFLbLczA8uecPYFPR/fNIAlta6nuNE21E7MbxiAkbv02BlGxUBC02nN/YH6Jn5gBMCNCzURSe0J0pr3nToN8uaGU4kaqkOcu4fnr6wv3/HbihkRer85tTnL2qunw2Y/M4MugZTo0yX82puzps+yAH3+ZTRiiviLYKgDLdcQYIjim3MRylujTU5qsubt/88lzBJF7OjPICHBDu/JGmbn+HuAOkd6lwqd7NfOgftpFQLPXJ4BlPadiLCQbYAJ4otQbwiaquIbAxQgYURZM5XyqvqEQdMQWy7yHPkBeJfNkYLdFNLP+HmVgZ32XvkBizL0na/IpwMNooMlbUykKpnEvIq7WcR9nyILAyee1UP2sgKxIr8kGA0ryd21lV60Z8mRklt6kkoTQG7Q2qmGllYhmLxQl1eiSLvFAo2QXLRsdqCy9WwbXuUpq05BGs=",
+    [string]$ChirpDllPath = $(Join-Path $PSScriptRoot "ChirpConnectNet-x64.dll"),
+    [string]$Text,
+    [int]$Sleep = 5,
+    [ValidateSet("UTF8", "UNICODE", "ASCII")]$Encoding = "UTF8"
+)
+ 
+[System.Reflection.Assembly]::LoadFile($ChirpDllPath)
+$connect = [ChirpConnectNet.ChirpConnect]::new($ChirpKey, $ChirpSecret, $ChirpConfig)
 
-$AssemblyPath = Join-Path $PSScriptRoot 'ChirpConnectNet-x64.dll'
-[System.Reflection.Assembly]::LoadFile($AssemblyPath)
-$connect = [ChirpConnectNet.ChirpConnect]::new($CHIRP_APP_KEY, $CHIRP_APP_SECRET, $CHIRP_APP_CONFIG)
-$err = $connect.Start([ChirpConnectNet.AudioMode]::Send)
-Write-Output $err
-$enc = [system.Text.Encoding]::UTF8
-
-while($true)
-{
-    $text = $enc.GetBytes("hello")
-    $connect.Send($text)
-    Start-Sleep -Seconds 5
+Register-ObjectEvent -InputObject $connect -EventName "OnSending" -Action {
+    Write-Host "OnSending" $Args
 }
+
+Register-ObjectEvent -InputObject $connect -EventName "OnSent" -Action {
+    Write-Host "OnSent" $Args
+}
+
+Register-ObjectEvent -InputObject $connect -EventName "OnStateChanged" -Action {
+    Write-Host "OnStateChanged" $Args
+}
+
+Register-ObjectEvent -InputObject $connect -EventName "OnUnsupportedRendererDevice" -Action {
+    Write-Host "OnUnsupportedRendererDevice" $Args
+}
+
+$err = $connect.Start([ChirpConnectNet.AudioMode]::Send)
+if ($err) { Write-Host $err }
+
+$payload = [system.Text.Encoding]::$Encoding.GetBytes($Text)
+
+try 
+{
+    while ($true) {
+        if ($connect.State -eq [ChirpConnectNet.SDKState]::Running)
+        {
+            $err = $connect.Send($payload)
+            if ($err) { Write-Host $err }
+        }
+        Start-Sleep -Seconds $Sleep
+    }
+}
+finally 
+{
+    $err = $connect.Stop()
+    if ($err) { Write-Host $err }
+}    
